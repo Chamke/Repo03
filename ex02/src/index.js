da6de1088ad8eeb4e314f77130d697f5218fced7
Chamke <terzimehic.amil@hotmail.com>
2022-03-30 16:20:33 +0200

    Added ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    //Only change code below this line
+    //Ovo je komentar
+    /*Ovo je drugi komentar */
+    //Only change code below this line
+}
+
+console.log(main());
+
+module.exports = main;
